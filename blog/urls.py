22aef750b5f165ebfdcd1024bf21d6d496446e28
sina03 <kevin.aubin03@gmail.com>
2019-05-29 16:57:49 +0200

    Ajout de vues qui permettent de créer et d'éditer un post

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail')
+    path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path(r'^post/new/$', views.post_new, name='post_new'),
+    path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
